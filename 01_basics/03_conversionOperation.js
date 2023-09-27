c7bb657f2b55fb10d423c7f669b02d3758710e73
Mihir Bhanushali <107618546+mihirrick@users.noreply.github.com>
2023-09-27 19:44:59 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "hitesh"
+
+// // console.log(typeof score)
+// // console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// //NaN type=> Not a Number
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let IsLoggedIn = 0
+
+// let ValueInIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(ValueInIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let SomeNumber = []
+
+let StringNumber = Array(SomeNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
