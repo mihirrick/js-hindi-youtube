65c8d721ca08deb6019ee40aa79c9331e0da3524
Mihir Bhanushali <107618546+mihirrick@users.noreply.github.com>
2023-09-30 14:43:53 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "hitesh "
+const repocount = "30 "
+
+// console.log(name + age + "Hero")
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+
+const gameName = new String('hiteshhc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
