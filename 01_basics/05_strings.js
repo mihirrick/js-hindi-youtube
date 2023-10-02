722c2a6df7eb7466be658d08c7b8fc884720ee76
Mihir Bhanushali <107618546+mihirrick@users.noreply.github.com>
2023-10-02 15:19:06 +0000

    number and maths

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,10 +5,29 @@ const repocount = "30 "
 
 console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
 
-const gameName = new String('hiteshhc')
+const gameName = new String('hitesh-hc-com')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 
 console.log(gameName.length)
 console.log(gameName.toUpperCase())
+console.log(gameName.charAt('2'))
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8, 4)
+
+const newStringOne = "   hitesh   "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('hitesh'))
+
+console.log(gameName.split('-'))
